0cf02e15a80b3858a6d7a46ccadff9ec157ba3e8
my <1782851229@qq.com>
2020-06-15 18:57:26 +0800

    third commit

--- js/set_roots.js
+++ js/set_roots.js
@@ -1,10 +1,9 @@
 (function(){
   var calc = function(){
     var docElement = document.documentElement;
-    var clientWidthValue = docElement.clientWidth>750? 750 : docElement.clientWidth;
+    var clientWidthValue = docElement.clientWidth>750 ? 750 : docElement.clientWidth;
     docElement.style.fontSize = 20*(clientWidthValue/375)+'px';
-  }
- calc();
+  calc();
    window.addEventListener('resize',calc);
 
 })();
